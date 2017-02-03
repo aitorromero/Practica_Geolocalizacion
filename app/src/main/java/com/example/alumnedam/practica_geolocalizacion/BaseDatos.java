70e2e2e14ad0c8638e675670dd8cf722f1168f1f
aitorromero <romeroaitor.sgr@gmail.com>
2017-02-03 18:58:17 +0100

    Cosa

--- app/src/main/java/com/example/alumnedam/practica_geolocalizacion/BaseDatos.java
+++ app/src/main/java/com/example/alumnedam/practica_geolocalizacion/BaseDatos.java
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class BaseDatos extends SQLiteOpenHelper{
 
     String sqlCreate;
-    String sqlInsert;
+    String[] sqlInsert;
 
     public BaseDatos(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -16,11 +16,21 @@ public class BaseDatos extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqlCreate= "CREATE TABLE AGAPITO (id_bus String, contrasena String, hora_inicio text, hora_fin text)";
+        sqlCreate= "CREATE TABLE AGAPITO (id_bus String, contrasena String)";
+
+        sqlInsert[0]="INSERT INTO AGAPITO ('id_bus', 'contrasena') VALUES ('1111A', '1111A')";
+        sqlInsert[1]="INSERT INTO AGAPITO ('id_bus', 'contrasena') VALUES('2222B', '2222B', '0', '0');";
+        sqlInsert[2]="INSERT INTO AGAPITO ('id_bus', 'contrasena') VALUES('3333C', '3333C');";
+
+        sqLiteDatabase.execSQL(sqlCreate);
+
+        for (int i=0; i < sqlInsert.length; i++){
+            sqLiteDatabase.execSQL(sqlInsert[i]);
+        }
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        
+
     }
 }
--- app/src/main/java/com/example/alumnedam/practica_geolocalizacion/MainActivity.java
+++ app/src/main/java/com/example/alumnedam/practica_geolocalizacion/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.alumnedam.practica_geolocalizacion;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BaseDatos bd = new BaseDatos(this, "Autobuseros", null, 2);
+        SQLiteDatabase sql= bd.getWritableDatabase();
     }
+
+
+
+
+
 }
